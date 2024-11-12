0f7f6957991eef1a4f717719806951a4d3adb6ef
arun <ptech784@gmail.com>
2024-11-12 20:42:55 +0530

    fix:format

--- prices/prices.go
+++ prices/prices.go
@@ -15,9 +15,10 @@ type TaxincludedPriceJob struct {
 
 func (job *TaxincludedPriceJob) Process() {
 	job.LoadData()
-	result := make(map[string]float64)
+	result := make(map[string]string)
 	for _, price := range job.InputPrices {
-		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+		taxIncludedPrice := price * (1 + job.TaxRate)
+		result[fmt.Sprintf("%.2f", price)] = fmt.Sprintf("%.2f", taxIncludedPrice)
 	}
 
 	fmt.Println(result)
